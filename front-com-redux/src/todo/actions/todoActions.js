8eb41228649078f043130fbfce9bcf2a1858fb78
Camila <camilanobre170@gmail.com>
2024-10-15 15:13:40 -0300

    correcao regex

--- front-com-redux/src/todo/actions/todoActions.js
+++ front-com-redux/src/todo/actions/todoActions.js
@@ -10,7 +10,7 @@ export const changeDescription = event => ({
 export const search = () => {
     return (dispatch, getState) => {
         const description = getState().todo.description
-        const search = description ? `&description__regex=/${description}/` : ''
+        const search = description ? `&description__regex=${description}` : "";
         const request = axios.get(`${URL}?sort=-createdAt${search}`)
             .then(resp => dispatch({ type: 'TODO_SEARCHED', payload: resp.data}))
     }
